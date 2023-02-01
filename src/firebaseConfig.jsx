7a3a9bb547e0a48a5de17f78c28fe2b37abd63cf
Arshad Ali <61376888+ARSHADKHAN615@users.noreply.github.com>
2023-02-01 23:45:31 +0530

    firebase config change

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { GoogleAuthProvider, getAuth } from "firebase/auth";
+import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
   apiKey: "AIzaSyASS-IP57NbSoEEE1BFvi9ZjAKeMzN9uzE",
@@ -7,10 +8,12 @@ const firebaseConfig = {
   projectId: "resume-api-75ad4",
   storageBucket: "resume-api-75ad4.appspot.com",
   messagingSenderId: "944723526338",
-  appId: "1:944723526338:web:9b7d3d03a5bf42433b315b"
+  appId: "1:944723526338:web:9b7d3d03a5bf42433b315b",
+  measurementId: "G-2VVS5WMBW4"
 };
 
 // Initialize Firebase
 export const FirebaseApp = initializeApp(firebaseConfig);
 export const auth = getAuth(FirebaseApp);
 export const provider = new GoogleAuthProvider();
+const analytics = getAnalytics(FirebaseApp);
